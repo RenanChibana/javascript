ac8bbd2f2e573627273d718f78b34de238a53689
Renan Chibana <renanchibana@gmail.com>
2025-04-03 11:25:45 -0300

    att aula011

--- aula011/ex012.js
+++ aula011/ex012.js
@@ -1,3 +1,7 @@
-var vel = 75.8
+var vel = 85.8
 
 console.log(`A velocidade do carro é de ${vel} km/h.`);
+if (vel > 90) {
+    console.log(`Você ultrapassou o limite de velocidade que é de 90 km/h, você foi Multado.`);
+}
+console.log(`Dirija sempre com o cinto de segurança, boa viagem!`);
---
+++ aula011/ex013.js
@@ -0,0 +1,7 @@
+var pais = 'Japão'
+
+if (pais == 'Brasil') {
+    console.log(`Você é brasileiro!!`);    
+} else {
+    console.log(`Você é estrangeiro, seja bem vindo ao Brasil!!`);    
+}
