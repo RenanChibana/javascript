b5b7e422fd7d9a469c6ea58b81bccf87a4411b7c
Renan Chibana <renanchibana@gmail.com>
2025-04-07 15:36:10 -0300

    att ex022

---
+++ aula014ex/ex022/script.js
@@ -0,0 +1,20 @@
+function tabuada() {
+    var num = document.getElementById('num')
+    var tab = document.getElementById('seltab')
+    document.writeln = `Tabuada do ${num.value}:<br>`
+    
+
+    if (num.value.length == 0) {
+        window.alert('[ERRO] Digite um valor válido para gerar a tabuada.')
+    } else {
+        var n = Number(num.value)
+        var r = 0
+        tab.innerHTML = ''
+        for(c = 0; c <= 10; c++) {
+            r = n * c
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${r}`
+            tab.appendChild(item)
+        }
+    }
+}
