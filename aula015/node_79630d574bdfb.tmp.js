e7957e06911e2fd5d2bcb10b7859c11de17e45f4
Renan Chibana <renanchibana@gmail.com>
2025-04-08 10:06:43 -0300

    aula015

---
+++ aula015/node_79630d574bdfb.tmp.js
@@ -0,0 +1,16 @@
+let num = [2, 3, 6, 8, 1]
+num[3] = 5
+num[5] = 9
+num.push(4)
+num[1] = 7
+
+
+console.log(num)
+console.log(`O primeiro valor do array é o ${num[0]}`);
+console.log(`Nosso vetor em ordem crescente é ${num.sort()}`);
+console.log(`O tamanho do array é de ${num.length} elementos.`);
+
+for (c = 0; c = 7; c++) {
+    console.log(num[c]);
+    
+}
