60bdee6e2d139920449c74b861cd47fc0a9562e6
Renan Chibana <renanchibana@gmail.com>
2025-04-11 10:21:35 -0300

    att ex016

--- aula016ex copy/script.js
+++ aula016ex copy/script.js
@@ -28,4 +28,38 @@ function add() {
     } else {
         window.alert('Número inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (list.length == 0) {
+        window.alert('Adicione valores antes de finalizar.')
+    } else {
+        let tot = list.length 
+        let soma = 0
+        let mai = 0
+        let men = 0
+        for (let pos in list) {
+            soma += list[pos]
+            if (pos == 0) {
+                mai = list[pos]
+                men = list[pos]
+            } else {
+                if (list[pos] > mai) {
+                    mai = list[pos]
+                }
+                if (list[pos] < men) {
+                    men = list[pos]
+                }
+            }
+        }
+        let med = soma / tot
+        res.innerHTML = ''   
+        res.innerHTML += `<br>Ao todo temos ${tot} números cadastrados.`
+        res.innerHTML += `<br>O maior valor foi ${mai}.`
+        res.innerHTML += `<br>O menor valor foi ${men}.`
+        res.innerHTML += `<br>A soma de todos os valores é ${soma}.`
+        res.innerHTML += `<br>A média dos valores é de ${med}.`
+        }
 }
